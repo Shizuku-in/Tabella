15fe6c9e428f7c8202fb705c3f567e50b7cd8036
gkouen <mcrleo@foxmail.com>
2026-06-07 15:14:17 +0800

    feat(web): i18n 支持

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,6 +3,8 @@ import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+import './i18n.ts'
+
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
